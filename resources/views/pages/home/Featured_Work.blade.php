e34392a63223f72675fbd3017a803295e01c28b1
steeven-js <jacques.steeven@gmail.com>
2023-06-06 06:56:41 -0400

    Case Study Réajustements

--- resources/views/pages/home/Featured_Work.blade.php
+++ resources/views/pages/home/Featured_Work.blade.php
@@ -19,7 +19,63 @@
             <div
                 class="relative grid items-center gap-8 px-4 pt-5 overflow-hidden shadow-sm rounded-2xl bg-slate-50 pb-14 shadow-sky-100/50 ring-1 ring-slate-100 sm:gap-12 sm:px-8 sm:pt-8 lg:grid-cols-12 lg:px-0 lg:py-0 xl:gap-16 xl:pt-16 dark:bg-gray-600">
                 <!-- Light gradient backdrop -->
-                <svg width="1280" height="492" viewBox="0 0 1280 492" fill="none" xmlns="http://www.w3.org/2000/svg"
+                <img src="storage/images/case-study-bg.svg" alt=""
+                    class="absolute inset-x-0 w-full h-full rotate-90 top-6 lg:rotate-0" />
+
+                <!-- Content -->
+                <div
+                    class="relative order-2 px-1 sm:px-4 lg:order-1 lg:col-span-6 lg:pt-16 lg:pb-16 lg:pl-12 xl:col-span-5 xl:pt-8 xl:pb-24 xl:pl-16">
+                    <div
+                        class="inline-flex items-center gap-2.5 text-sm font-medium leading-[16px] text-sky-900/80 sm:text-md dark:text-sky-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="currentColor" class="w-4 h-4 text-sky-900/75 dark:text-sky-600">
+                            <g stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75"
+                                transform="translate(0.5 0.5)" fill="none" stroke="currentColor">
+                                <polyline points="8.333,14 1,14 1,1 23,1 23,14 20,14 "></polyline>
+                                <line x1="13" y1="14" x2="19" y2="20" stroke="currentColor">
+                                </line>
+                                <polygon points=" 6,7 10,19 13,14 18,11 " stroke="currentColor"></polygon>
+                            </g>
+                        </svg>
+
+                        Web Development
+                    </div>
+
+                    <h3 class="mt-5 text-2xl font-medium font-display text-slate-900 sm:mt-6 sm:text-3xl">
+                        Bright School
+                    </h3>
+
+                    <p class="mt-3 leading-8 text-md text-slate-700 sm:mt-4 sm:text-base sm:leading-8 dark:text-white">
+                        Revitalizing Bright School's online presence with a redesigned
+                        website, branding, and Google Ads campaign, resulting in a 200%
+                        enrollment increase.
+                    </p>
+
+                    <a href="case-study.html"
+                        class="flex items-center gap-2 text-sm font-medium duration-200 ease-in-out group mt-14 text-sky-600 hover:text-sky-700 sm:mt-16 sm:text-md">
+                        View Case Study
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                            class="h-5 w-5 text-sky-500 duration-300 ease-in-out group-hover:-translate-y-0.5 group-hover:translate-x-0.5 group-hover:text-sky-600">
+                            <path fill-rule="evenodd"
+                                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </div>
+
+                <!-- Image -->
+                <a href="case-study.html"
+                    class="relative order-1 w-full h-full overflow-hidden group aspect-w-16 aspect-h-9 rounded-2xl ring-1 ring-slate-100/75 lg:order-2 lg:col-span-6 lg:rounded-r-none lg:rounded-l-none xl:col-span-7 xl:rounded-tl-2xl">
+                    <img src="storage/images/case_studies/case-study-thumbnail-04.jpg" alt="Wavvy"
+                        class="absolute inset-x-0 bottom-0 object-cover object-top w-full h-full transition duration-300 group-hover:scale-105" />
+                </a>
+            </div>
+            <!-- 2nd Case Study -->
+            <div
+                class="relative grid items-center gap-8 px-4 pt-5 overflow-hidden shadow-sm rounded-2xl bg-slate-50 pb-14 shadow-sky-100/50 ring-1 ring-slate-100 sm:gap-12 sm:px-8 sm:pt-8 lg:grid-cols-12 lg:px-0 lg:py-0 xl:gap-16 xl:pt-16 dark:bg-gray-600">
+                <!-- Light gradient backdrop -->
+                <svg width="1280" height="492" viewBox="0 0 1280 492" fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
                     class="absolute inset-x-0 w-full h-full rotate-90 top-6 lg:rotate-0">
                     <g clip-path="url(#clip0_313_58)">
                         <rect width="1280" height="492" />
@@ -54,96 +110,17 @@
                             fill="currentColor" class="w-4 h-4 text-sky-900/75 dark:text-sky-600">
                             <g stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75"
                                 transform="translate(0.5 0.5)" fill="none" stroke="currentColor">
-                                <path d="M10,11.01l8.378-8.389a2.122,2.122,0,0,1,3,0h0a2.122,2.122,0,0,1,0,3L13,14"
-                                    stroke="currentColor"></path>
-                                <path
-                                    d="M7.514,11.941a3.214,3.214,0,1,1,4.545,4.545C10.8,17.74,6,18,6,18S6.26,13.2,7.514,11.941Z"
-                                    stroke="currentColor"></path>
-                                <polyline points="12 2 2 2 2 22 22 22 22 12"></polyline>
-                            </g>
-                        </svg>
-
-                        Branding
-                    </div>
-
-                    <h3 class="mt-5 text-2xl font-medium font-display text-slate-900 sm:mt-6 sm:text-3xl">
-                        Horizons in Perspective
-                    </h3>
-
-                    <p class="mt-3 leading-8 text-md text-slate-700 sm:mt-4 sm:text-base sm:leading-8 dark:text-white">
-                        I helped Horizons in Perspective develop a new branding and
-                        website experience, as well as an engaging product design for
-                        their bi-annual journal publication.
-                    </p>
-
-                    <a href="case-study.html"
-                        class="flex items-center gap-2 text-sm font-medium duration-200 ease-in-out group mt-14 text-sky-600 hover:text-sky-700 sm:mt-16 sm:text-md">
-                        View Case Study
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                            class="h-5 w-5 text-sky-500 duration-300 ease-in-out group-hover:-translate-y-0.5 group-hover:translate-x-0.5 group-hover:text-sky-600">
-                            <path fill-rule="evenodd"
-                                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                </div>
-
-                <!-- Image -->
-                <a href="case-study.html"
-                    class="relative order-1 w-full h-full overflow-hidden group aspect-w-16 aspect-h-9 rounded-2xl ring-1 ring-slate-100/75 lg:order-2 lg:col-span-6 lg:rounded-r-none lg:rounded-l-none xl:col-span-7 xl:rounded-tl-2xl">
-                    <img src="storage/images/case_studies/case-study-thumbnail-01.jpg" alt="Horizons in Perspective"
-                        class="absolute inset-x-0 bottom-0 object-cover object-top w-full h-full transition duration-300 group-hover:scale-105" />
-                </a>
-            </div>
-
-            <!-- 2nd Case Study -->
-            <div
-                class="relative grid items-center gap-8 px-4 pt-5 overflow-hidden shadow-sm rounded-2xl bg-slate-50 pb-14 shadow-sky-100/50 ring-1 ring-slate-100 sm:gap-12 sm:px-8 sm:pt-8 lg:grid-cols-12 lg:px-0 lg:py-0 xl:gap-16 xl:pt-16 dark:bg-gray-600">
-                <!-- Light gradient backdrop -->
-                <svg width="1280" height="492" viewBox="0 0 1280 492" fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="absolute inset-x-0 w-full h-full rotate-90 top-6 lg:rotate-0">
-                    <g clip-path="url(#clip0_313_58)">
-                        <rect width="1280" height="492" />
-                        <g opacity="0.35" filter="url(#filter0_f_313_58)">
-                            <path d="M660.4 572.997H789V332.155L482 491.521L660.4 572.997Z" fill="#60A5FA"
-                                fill-opacity="0.45" />
-                            <path d="M789 332.155V274H549H389L482 491.521L789 332.155Z" fill="#7DD3FC"
-                                fill-opacity="0.8" />
-                            <path d="M389 572.997H660.4L482 491.521L389 274V572.997Z" fill="#F0FDFA"
-                                fill-opacity="0.5" />
-                        </g>
-                    </g>
-                    <defs>
-                        <filter id="filter0_f_313_58" x="38.07" y="-76.9299" width="1101.86"
-                            height="1000.86" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                            <feGaussianBlur stdDeviation="175.465" result="effect1_foregroundBlur_313_58" />
-                        </filter>
-                        <clipPath id="clip0_313_58">
-                            <rect width="1280" height="492" fill="white" />
-                        </clipPath>
-                    </defs>
-                </svg>
-
-                <!-- Content -->
-                <div
-                    class="relative order-2 px-1 sm:px-4 lg:order-1 lg:col-span-6 lg:pt-16 lg:pb-16 lg:pl-12 xl:col-span-5 xl:pt-8 xl:pb-24 xl:pl-16">
-                    <div
-                        class="inline-flex items-center gap-2.5 text-sm font-medium leading-[16px] text-sky-900/80 sm:text-md dark:text-sky-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="currentColor" class="w-4 h-4 text-sky-900/75 dark:text-sky-600">
-                            <g stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75"
-                                transform="translate(0.5 0.5)" fill="none" stroke="currentColor">
                                 <rect x="2" y="2" width="20" height="20"></rect>
                                 <polyline points="7 8 11 12 7 16" stroke="currentColor"></polyline>
                                 <line x1="13" y1="8" x2="17" y2="8"
-                                    stroke="currentColor"></line>
+                                    stroke="currentColor">
+                                </line>
                                 <line x1="13" y1="16" x2="17" y2="16"
-                                    stroke="currentColor"></line>
+                                    stroke="currentColor">
+                                </line>
                                 <line x1="15" y1="12" x2="17" y2="12"
-                                    stroke="currentColor"></line>
+                                    stroke="currentColor">
+                                </line>
                             </g>
                         </svg>
 
@@ -245,8 +222,32 @@
             <div
                 class="relative grid items-center gap-8 px-4 pt-5 overflow-hidden shadow-sm rounded-2xl bg-slate-50 pb-14 shadow-sky-100/50 ring-1 ring-slate-100 sm:gap-12 sm:px-8 sm:pt-8 lg:grid-cols-12 lg:px-0 lg:py-0 xl:gap-16 xl:pt-16 dark:bg-gray-600">
                 <!-- Light gradient backdrop -->
-                <img src="storage/images/case-study-bg.svg" alt=""
-                    class="absolute inset-x-0 w-full h-full rotate-90 top-6 lg:rotate-0" />
+                <svg width="1280" height="492" viewBox="0 0 1280 492" fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="absolute inset-x-0 w-full h-full rotate-90 top-6 lg:rotate-0">
+                    <g clip-path="url(#clip0_313_58)">
+                        <rect width="1280" height="492" />
+                        <g opacity="0.35" filter="url(#filter0_f_313_58)">
+                            <path d="M660.4 572.997H789V332.155L482 491.521L660.4 572.997Z" fill="#60A5FA"
+                                fill-opacity="0.45" />
+                            <path d="M789 332.155V274H549H389L482 491.521L789 332.155Z" fill="#7DD3FC"
+                                fill-opacity="0.8" />
+                            <path d="M389 572.997H660.4L482 491.521L389 274V572.997Z" fill="#F0FDFA"
+                                fill-opacity="0.5" />
+                        </g>
+                    </g>
+                    <defs>
+                        <filter id="filter0_f_313_58" x="38.07" y="-76.9299" width="1101.86"
+                            height="1000.86" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                            <feGaussianBlur stdDeviation="175.465" result="effect1_foregroundBlur_313_58" />
+                        </filter>
+                        <clipPath id="clip0_313_58">
+                            <rect width="1280" height="492" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
 
                 <!-- Content -->
                 <div
@@ -257,24 +258,26 @@
                             fill="currentColor" class="w-4 h-4 text-sky-900/75 dark:text-sky-600">
                             <g stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75"
                                 transform="translate(0.5 0.5)" fill="none" stroke="currentColor">
-                                <polyline points="8.333,14 1,14 1,1 23,1 23,14 20,14 "></polyline>
-                                <line x1="13" y1="14" x2="19" y2="20"
-                                    stroke="currentColor"></line>
-                                <polygon points=" 6,7 10,19 13,14 18,11 " stroke="currentColor"></polygon>
+                                <path d="M10,11.01l8.378-8.389a2.122,2.122,0,0,1,3,0h0a2.122,2.122,0,0,1,0,3L13,14"
+                                    stroke="currentColor"></path>
+                                <path
+                                    d="M7.514,11.941a3.214,3.214,0,1,1,4.545,4.545C10.8,17.74,6,18,6,18S6.26,13.2,7.514,11.941Z"
+                                    stroke="currentColor"></path>
+                                <polyline points="12 2 2 2 2 22 22 22 22 12"></polyline>
                             </g>
                         </svg>
 
-                        Web Development
+                        Branding
                     </div>
 
                     <h3 class="mt-5 text-2xl font-medium font-display text-slate-900 sm:mt-6 sm:text-3xl">
-                        Bright School
+                        Horizons in Perspective
                     </h3>
 
                     <p class="mt-3 leading-8 text-md text-slate-700 sm:mt-4 sm:text-base sm:leading-8 dark:text-white">
-                        Revitalizing Bright School's online presence with a redesigned
-                        website, branding, and Google Ads campaign, resulting in a 200%
-                        enrollment increase.
+                        I helped Horizons in Perspective develop a new branding and
+                        website experience, as well as an engaging product design for
+                        their bi-annual journal publication.
                     </p>
 
                     <a href="case-study.html"
@@ -292,7 +295,7 @@
                 <!-- Image -->
                 <a href="case-study.html"
                     class="relative order-1 w-full h-full overflow-hidden group aspect-w-16 aspect-h-9 rounded-2xl ring-1 ring-slate-100/75 lg:order-2 lg:col-span-6 lg:rounded-r-none lg:rounded-l-none xl:col-span-7 xl:rounded-tl-2xl">
-                    <img src="storage/images/case_studies/case-study-thumbnail-04.jpg" alt="Wavvy"
+                    <img src="storage/images/case_studies/case-study-thumbnail-01.jpg" alt="Horizons in Perspective"
                         class="absolute inset-x-0 bottom-0 object-cover object-top w-full h-full transition duration-300 group-hover:scale-105" />
                 </a>
             </div>
