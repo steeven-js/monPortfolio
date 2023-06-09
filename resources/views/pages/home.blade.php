67117ecbb4dbca1d64452b0b76ac24a7d0ab0023
steeven-js <jacques.steeven@gmail.com>
2023-06-09 19:08:00 -0400

    work in progress

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,14 +9,14 @@
     <!--2 Experience end-->
 
     <!--3 Row_of_Tech_Icons start-->
-    @include('pages/home/row_of_Tech_Icons')
+    @include('pages/home/row_of_tech_icons')
     <!--3 Row_of_Tech_Icons end-->
 
     <!--4 Featured_Work start-->
-    @include('pages/home/featured_Work')
+    @include('pages/home/featured_work')
     <!--4 Featured_Work end-->
 
     <!--5 Featured_Blog_Articles start-->
-    {{-- @include('pages/home/Featured_Blog_Articles') --}}
+    {{-- @include('pages/home/featured_blog_articles') --}}
     <!--5 Featured_Blog_Articles end-->
 @endsection
